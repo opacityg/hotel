d1f3170a181c0f591ed2721009c981f8b8087b0d
DESKTOP-4B9JQ0A\ASUS <1072686087@qq.com>
2020-03-15 23:31:29 +0800

    搜索頁面的添加

--- hotel/src/components/keyWords.vue
+++ hotel/src/components/keyWords.vue
@@ -1,20 +1,172 @@
 <template>
   <div class="keyword-container">
       <div class="nav">
-          <div>
-            <i class="iconfont icon-icon_page_return1"></i>
+          <div class="nav-bar">
+            <i class="iconfont icon-icon_page_return1 return"></i>
+            <div class="search-bar">
+              <div class="search-bar-left">
+                深圳
+              </div>
+              <div class="search-field-box">
+                <i class="iconfont icon-search icon-icom_sousuo"></i>
+                <input type="search" placeholder="请输入搜索关键词" class="search-field">
+              </div>
 
+            </div>
           </div>
+      </div>
+      <div class="category">
+        <!--左側分類-->
+        <div class="scroll-view-left" >
+          <div class="scroll-view-left-item"
+               v-for="(item, index) in lists" :key="index"
+               :class="activeIndex == index ? 'category-active' : ''"
+               @click="handleSelectedType(index)"
+          >
+            {{item.categoryName}}
+          </div>
+        </div>
+
+        <div class="scroll-view-right">
+          <div class="scroll-view-right-item" v-for="(child,index) in subCategory" :key="index">
+            {{child}}
+          </div>
+        </div>
       </div>
   </div>
 </template>
 
 <script>
   export default {
-    name: 'keyWords'
+    name: 'keyWords',
+    data(){
+      return{
+        activeIndex:0,
+        lists:[
+          {
+            categoryName:'特色'
+          },
+          {
+            categoryName:'商圈'
+          },
+          {
+            categoryName:'机场车站'
+          },
+          {
+            categoryName:'景点'
+          },
+          {
+            categoryName:'行政区'
+          },
+        ],
+        subCategory:['温泉','青年旅舍','休闲度假','民宿客栈','精品酒店']
+      }
+    },
+    methods:{
+      handleSelectedType(){
+        console.log('sssssssss')
+      }
+    }
   }
 </script>
 
 <style scoped>
+.nav{
+  width: 100%;
+  height: 44px;
+  background:linear-gradient(225deg,rgba(251,88,112,1) 0%,rgba(251,143,96,1) 100%);
+  padding:0 15px;
+}
+.nav-bar{
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  height: 44px;
 
+}
+  .return{
+    color: #fff;
+    font-size: 15px;
+    margin-right: 15px;
+  }
+  .search-bar{
+   display: flex;
+    flex-direction: row;
+
+  }
+  .search-bar-left{
+    width: 50px;
+    height: 30px;
+    border-radius: 15px 0 0 15px;
+    font-size:16px;
+    color: #434A54;
+    background: #fff;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    display: flex;
+    margin-right: 1.5px;
+  }
+  .search-field{
+    width: 100%;
+    height: 30px;
+    border-radius:0px 30px 30px 0px;
+    background-size: 50% auto;
+    background-origin: border-box;
+    background: #fff;
+    padding-left: 31px;
+    box-sizing: border-box;
+    /*background-img: url(../../static/keyWords/search.png) no-repeat;*/
+  }
+  .search-field-box{
+    position: relative;
+    width: 270px;
+    height: 30px;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+  }
+  .icon-search{
+    position: absolute;
+    font-size: 15px;
+    font-weight: bold;
+    color: #A0A9B5;
+    margin-left: 15px;
+    margin-right: 5px;
+
+  }
+  .category{
+    display: flex;
+    flex-direction: row;
+
+  }
+  .scroll-view-left{
+    width:84px;
+    background: #FAFAFA;
+  }
+  .scroll-view-left-item{
+    width:84px;
+    height:40px;
+    font-size:14px;
+    font-weight:400;
+    color:#60656E;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+  }
+  .category-active{
+    color:rgba(251,97,110,1);
+    background: #fff;
+  }
+  .scroll-view-right-item{
+    width: 207px;
+    height: 39.5px;
+    line-height: 39.5px;
+    border-bottom: 1px solid #F6F6F6;
+    margin-left: 19.5px;
+    font-size:14px;
+    font-weight:400;
+    color:rgba(96,101,110,1);
+  }
 </style>
--- hotel/src/router/index.js
+++ hotel/src/router/index.js
@@ -1,15 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/home'
+import keyWords  from '@/components/keyWords'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
+      path: '/home',
       name: 'Home',
       component: Home
+    },
+    {
+      path: '/',
+      name: 'keyWords',
+      component: keyWords
     }
   ]
 })
